6800631a4258e41fdc555196804262ad1da7e9dc
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-05 21:20:09 +0600

    user table design

--- Django_Sales_Inventory_Database_Design/sales_inventory/models.py
+++ Django_Sales_Inventory_Database_Design/sales_inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField()
+    password = models.CharField(max_length=500)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    otp = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
