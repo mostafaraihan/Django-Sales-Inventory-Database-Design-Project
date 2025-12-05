5b369d108b10a9244bc45c1bf1e8a190e71c2dcb
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-05 21:44:04 +0600

    Customer model create

--- Django_Sales_Inventory_Database_Design/sales_inventory/models.py
+++ Django_Sales_Inventory_Database_Design/sales_inventory/models.py
@@ -19,11 +19,20 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places= 2)  #most important (decimal_places) part
     unit = models.CharField(max_length=100)
     username = models.ForeignKey(User, on_delete=models.CASCADE,related_name='products')
     category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    mobile = models.CharField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE,related_name='customers')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
